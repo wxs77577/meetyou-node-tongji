536ad7325ca2c0b26f44e6a39358708cb1ffa11d
linweiwei <linweiwei@xiaoyouzi.com>
2017-12-28 10:05:15 +0800

    增加日志输出

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 
 const config = {
-    username: '', //你登录百度统计时候用的账号
-    password: '', //你的密码，放心我不会盗号
-    token: '', //你的token，在百度统计你的控制台页面里能找到
-    uuid: '' //随便起个就行了
+    username: '', //机密
+    password: '', //机密
+    token: '', //机密
+    uuid: 'this-is-a-fucking-uuid' //随便起个就行了
 }
 
 const tongji = require('./index').getInstance(config);
